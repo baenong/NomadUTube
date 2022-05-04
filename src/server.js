0d423bd02266b4d111bfcbdabf6c907181c285c1
Baenong <anminsnusa@gmail.com>
2022-05-04 17:27:11 +0900

    Create morgan

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
+const handleHome = (req, res) => {
+  return res.send("Test");
+};
+
+// Global Middlewares
+app.use(logger);
+
+// GET
+app.get("/", handleHome);
+
+// Listening Request
 const handleListening = () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 };
